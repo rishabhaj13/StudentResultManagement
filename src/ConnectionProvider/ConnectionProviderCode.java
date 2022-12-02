69588daa3d41ca6ef8975ea822f304ba99a5f5ac
Rishabh <rishabhaj13@gmail.com>
2022-12-02 13:34:25 +0530

    first commit

---
+++ src/ConnectionProvider/ConnectionProviderCode.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConnectionProvider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author Rishabh
+ */
+public class ConnectionProviderCode {
+    
+    public static Connection getcon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentresultsystem", "root", "654321" );
+            return con;
+            
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.toString());
+            return null;
+        }
+    }
+    
+    public static void main(String args[]){
+        ConnectionProviderCode con = new ConnectionProviderCode();
+        Connection c = con.getcon();
+    }
+    
+}
